b93a00587702b31a6835b6be34a677617db47387
AbdulrahmanDaud10 <dulisyke3015@gmail.com>
2023-05-10 22:10:20 +0300

    customer repo

--- pkg/api/model.go
+++ pkg/api/model.go
@@ -8,12 +8,13 @@ type Model struct {
 	UpdatedAt time.Time `gorm:"column:updated_at;type:datetime;not null;" json:"updated_at"`
 }
 
-type customer struct {
+type Customer struct {
 	Model
 	customerID  string `gorm:"column:customer_id;" json:"customer_id"`
-	userName    string `gorm:"column:"user_name;" json:"user_name"`
-	code        string `gorm:"column:"code;" json:"code"`
-	email       string `gorm:"column:"email;" json:"email"`
-	password    string `gorm:"column:"password;" json:"password"`
-	phoneNumber string `gorm:"column:"phone_number;" json:"phone_number"`
+	order       string `gorm:"column:order;" json:"order"`
+	userName    string `gorm:"column:user_name;" json:"user_name"`
+	code        string `gorm:"column:code;" json:"code"`
+	email       string `gorm:"column:email;" json:"email"`
+	password    string `gorm:"column:password;" json:"password"`
+	phoneNumber string `gorm:"column:phone_number;" json:"phone_number"`
 }
--- pkg/repository/customer_repository.go
+++ pkg/repository/customer_repository.go
@@ -1,8 +1,14 @@
 package repository
 
-import "gorm.io/gorm"
+import (
+	"fmt"
+
+	"github.com/AbdulrahmanDaud10/slade360-customer-order-service/pkg/api"
+	"gorm.io/gorm"
+)
 
 type Storage interface {
+	CreateOrder(customer api.Customer) error
 }
 
 type storage struct {
@@ -13,4 +19,15 @@ func NewStorage(db *gorm.DB) Storage {
 	return &storage{
 		db: db,
 	}
+}
+
+func (s *storage) CreateCustomerService(customer api.Customer) error {
+	return nil
+}
+
+func (s *storage) CreateOrder(customer api.Customer) error {
+	if customer := s.db.Create(&customer); customer.Error != nil {
+		return fmt.Errorf(customer.Error.Error())
+	}
+	return nil
 }
